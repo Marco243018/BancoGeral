b0d22216bda76dec4969ff9f85be4f646242f5ea
Luck243 <marcoantoniodasilvafurtado@gmail.com>
2024-04-25 15:18:11 -0300

    initial commit

---
+++ Entitites/Agencia.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace AppBanco.Entitites
+{
+    public class Agencia
+    {
+        public string Numero{get; set;}
+        public string NomeAgencia{get; set;}
+        public string Endereco{get; set;}
+        public int EnderecoNumero{get; set;}
+        public string Bairro{get; set;}
+        public string Cidade{get; set;}
+        public string Estado {get; set;}
+        public List<Conta> Contas {get; set;}
+        public Agencia()
+        {
+            Contas = new List<Conta>();
+        }
+        public override string ToString()
+        {
+            string Retorno = "Número.........: " + Numero + "\n" +
+                             "Número Agência.: " + NomeAgencia + "\n";
+            return Retorno;
+        }
+        public void AddConta(Conta c)
+        {
+            Contas.Add(c);
+        }
+        public void RemoveConta(int Index)
+        {
+            Contas.RemoveAt(Index);
+        }
+        public string CarregarAgenciaXML(string XMLAgencia)
+        {
+            return "";
+        }
+    }
+}
