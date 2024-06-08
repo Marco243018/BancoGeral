ed6ef32a5345ba39bc0e8c9f1630fbecce85a9fa
Luck243 <marcoantoniodasilvafurtado@gmail.com>
2024-06-08 09:38:24 -0300

    Aula 35 v1.01

--- Models/Enums.cs
+++ Models/Enums.cs
@@ -18,23 +18,3 @@ namespace AppBanco.Models
         }
     }
 }
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-namespace AppBanco.Models
-{
-    public class Enums
-    {
-        public enum TipoMovimento
-        {
-            Credito,
-            Debito
-        }        
-        public enum TipoConta
-        {
-            Corrente,
-            Poupanca
-        }
-    }
-}
